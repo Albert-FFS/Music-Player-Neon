a6d7d5834b8de047b5485ba620dd2ae5714fc295
Albert-FFS <r.a.s.m.13@hotmail.com>
2022-12-04 15:32:40 -0800

    Punto de Partida del proyecto Audios agregados Html y Css Preparados Javascript a escribir

---
+++ script.js
@@ -0,0 +1,35 @@
+const playHtml = '<i class="fas fa-play"></i>';
+const pauseHtml ='<i class="fas fa-pause"></i>';
+const path = 'C:/Users/Rasm1/OneDrive/Documentos/Vs CODE/Practicas WEB DESIGN/Music Player Neon/Media/';
+const list = [
+  {
+    Title:'Vaporwave Dimension',
+    Artist:'Reve Lucide',
+    Album:'TerminalMonatage',
+    Src:'Dimension LUNE.mp3'
+  },
+  {
+    Title:'Midnight City',
+    Artist:'M83',
+    Album:'Midnight City',
+    Src:'M83.mp3'
+  },
+  {
+    Title:'Otherside',
+    Artist:'Red Hot Chilli Pepper',
+    Album:'Californication',
+    Src:'Otherside.mp3'
+  },
+  {
+    Title:'Shadowtask',
+    Artist:'PYLOT',
+    Album:'Shadowtask',
+    Src:'Shadowtask.mp3'
+  },
+  {
+    Title:'Zombie',
+    Artist:'The Cranberries',
+    Album:'No Need to Argue',
+    Src:'Zombie.mp3'
+  }
+];
