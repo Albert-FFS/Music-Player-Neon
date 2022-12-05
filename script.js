03fd07286fe56531c70861010b9ef73e58968210
Albert-FFS <r.a.s.m.13@hotmail.com>
2022-12-04 17:03:21 -0800

    Boton de Play y Pause Funcional

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const playHtml = '<i class="fas fa-play"></i>';
 const pauseHtml ='<i class="fas fa-pause"></i>';
 const path = 'C:/Users/Rasm1/OneDrive/Documentos/Vs CODE/Practicas WEB DESIGN/Music Player Neon/Media/';
+const pathServer = 'http://127.0.0.1:5500/media/';
 const list = [
   {
     Title:'Vaporwave Dimension',
@@ -33,3 +34,42 @@ const list = [
     Src:'Zombie.mp3'
   }
 ];
+const Clip = document.querySelector('#Audio');
+const Name_Tag = document.querySelector('#Name_Song');
+const Artist_Tag = document.querySelector('#Artist_Song');
+const Album_Tag = document.querySelector('#Album_Song');
+const Play_Pause = document.querySelector('#Play-Pause');
+const pointer = 0;
+let IsItPlay = false;
+function PlayClip(){
+  console.log('Play');
+  Play_Pause.innerHTML = pauseHtml;
+  Clip.src = pathServer + list[pointer].Src ;
+  Clip.play();
+  IsItPlay = true;
+}
+function PauseClip(){
+  console.log('Pause');
+  Play_Pause.innerHTML = playHtml;
+  Clip.pause();
+  IsItPlay = false;
+}
+function Prev(){
+
+}
+function Now(){
+  if(IsItPlay){
+    PauseClip();
+  }else{
+    PlayClip();
+  }
+}
+function Next(){
+
+}
+function Mute(){
+
+}
+function Loop(){
+
+}
