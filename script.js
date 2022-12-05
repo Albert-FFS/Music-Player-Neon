f08f6ee54976b62e1aa9c0304c3157a5148c3866
Albert-FFS <r.a.s.m.13@hotmail.com>
2022-12-04 18:41:08 -0800

    Loop y Mute Funcional mas Cambios en aspectos

--- script.js
+++ script.js
@@ -1,88 +1,106 @@
 const playHtml = '<i class="fas fa-play"></i>';
-const pauseHtml ='<i class="fas fa-pause"></i>';
-const path = 'C:/Users/Rasm1/OneDrive/Documentos/Vs CODE/Practicas WEB DESIGN/Music Player Neon/Media/';
-const pathServer = 'http://127.0.0.1:5500/media/';
+const pauseHtml = '<i class="fas fa-pause"></i>';
+const muteHTML ='<i class="fas fa-volume-mute"></i>';
+const non_muteHtml ='<i class="fas fa-volume-off"></i>';
+const loopHtml ='<i class="fas fa-undo"></i>';
+const non_loopHtml ='<i class="fas fa-times"></i>';
+const path =
+  "C:/Users/Rasm1/OneDrive/Documentos/Vs CODE/Practicas WEB DESIGN/Music Player Neon/Media/";
+const pathServer = "http://127.0.0.1:5500/media/";
 const list = [
   {
-    Title:'Vaporwave Dimension',
-    Artist:'Reve Lucide',
-    Album:'TerminalMonatage',
-    Src:'Dimension LUNE.mp3'
+    Title: "Vaporwave Dimension",
+    Artist: "Reve Lucide",
+    Album: "TerminalMonatage",
+    Src: "Dimension LUNE.mp3",
   },
   {
-    Title:'Midnight City',
-    Artist:'M83',
-    Album:'Midnight City',
-    Src:'M83.mp3'
+    Title: "Midnight City",
+    Artist: "M83",
+    Album: "Midnight City",
+    Src: "M83.mp3",
   },
   {
-    Title:'Otherside',
-    Artist:'Red Hot Chilli Pepper',
-    Album:'Californication',
-    Src:'Otherside.mp3'
+    Title: "Otherside",
+    Artist: "Red Hot Chilli Pepper",
+    Album: "Californication",
+    Src: "Otherside.mp3",
   },
   {
-    Title:'Shadowtask',
-    Artist:'PYLOT',
-    Album:'Shadowtask',
-    Src:'Shadowtask.mp3'
+    Title: "Shadowtask",
+    Artist: "PYLOT",
+    Album: "Shadowtask",
+    Src: "Shadowtask.mp3",
   },
   {
-    Title:'Zombie',
-    Artist:'The Cranberries',
-    Album:'No Need to Argue',
-    Src:'Zombie.mp3'
-  }
+    Title: "Zombie",
+    Artist: "The Cranberries",
+    Album: "No Need to Argue",
+    Src: "Zombie.mp3",
+  },
 ];
-const Clip = document.querySelector('#Audio');
-const Name_Tag = document.querySelector('#Name_Song');
-const Artist_Tag = document.querySelector('#Artist_Song');
-const Album_Tag = document.querySelector('#Album_Song');
-const Play_Pause = document.querySelector('#Play-Pause');
+const Clip = document.querySelector("#Audio");
+const Name_Tag = document.querySelector("#Name_Song");
+const Artist_Tag = document.querySelector("#Artist_Song");
+const Album_Tag = document.querySelector("#Album_Song");
+const Play_Pause = document.querySelector("#Play-Pause");
+const MuteButton = document.querySelector('#Mute');
+const LoopButton = document.querySelector('#Loop');
 let pointer = 0;
 let IsItPlay = false;
-function PlayClip(){
-  console.log('Play');
+function PlayClip() {
   Play_Pause.innerHTML = pauseHtml;
-  Clip.src = pathServer + list[pointer].Src ;
+  Clip.src = pathServer + list[pointer].Src;
+  Artist_Tag.innerHTML = list[pointer].Artist;
+  Album_Tag.innerHTML = list[pointer].Album;
+  Name_Tag.innerHTML = list[pointer].Title;
   Clip.play();
   IsItPlay = true;
 }
-function PauseClip(){
-  console.log('Pause');
+function PauseClip() {
   Play_Pause.innerHTML = playHtml;
   Clip.pause();
   IsItPlay = false;
 }
-function Prev(){
-  if(pointer <= 0 ){
-    pointer = list.length-1;
-  }
-  else{
+function Prev() {
+  if (pointer <= 0) {
+    pointer = list.length - 1;
+  } else {
     pointer--;
   }
   PlayClip();
 }
-function Now(){
+function Now() {
   console.log(pointer);
-  if(IsItPlay){
+  if (IsItPlay) {
     PauseClip();
-  }else{
+  } else {
     PlayClip();
   }
 }
-function Next(){
-  if(pointer >= list.length-1){
+function Next() {
+  if (pointer >= list.length - 1) {
     pointer = 0;
-  }
-  else{
+  } else {
     pointer++;
   }
   PlayClip();
 }
-function Mute(){
-
+function Mute() {
+  if (Clip.muted) {
+    MuteButton.innerHTML = muteHTML;
+    Clip.muted = false;
+  } else {
+    MuteButton.innerHTML = non_muteHtml;
+    Clip.muted = true;
+  }
 }
-function Loop(){
-
+function Loop() {
+  if (Clip.loop) {
+    LoopButton.innerHTML =non_loopHtml;
+    Clip.loop = false;
+  } else {
+    LoopButton.innerHTML =loopHtml;
+    Clip.loop = true;
+  }
 }
